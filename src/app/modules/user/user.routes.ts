00293214c8f523bf653d2239797a3a3569f9fe08
MostakemHossain <mostakememon123@gmail.com>
2024-07-03 23:20:54 +0600

    developing middlewares validate request data

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
 import { userController } from "./user.controller";
+import { userValidation } from "./user.validation";
 const router = express.Router();
 
-router.post("/create-user", userController.createUser);
+router.post(
+  "/create-user",
+  validateRequest(userValidation.createUserValidationSchema),
+  userController.createUser
+);
 router.get("/", userController.getAllUser);
 router.get("/:userId", userController.getSingleUser);
 router.delete("/:userId", userController.deleteAUser);
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: "Name is required",
+    }),
+    email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email("Invalid email address"),
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(6, "Password must be at least 6 characters long")
+      .regex(
+        /(?=.*[a-z])/,
+        "Password must contain at least one lowercase letter"
+      )
+      .regex(
+        /(?=.*[A-Z])/,
+        "Password must contain at least one uppercase letter"
+      )
+      .regex(/(?=.*[0-9])/, "Password must contain at least one number")
+      .regex(
+        /(?=.*[!@#$%^&*])/,
+        "Password must contain at least one special character"
+      ),
+  }),
+});
+export const userValidation = {
+  createUserValidationSchema,
+};
